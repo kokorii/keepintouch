8157c4724c67a06ed29d6d24abec8e2372843577
kokori <kokori91@gmail.com>
2021-10-28 00:36:49 +0900

    전체 완성본

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telegram
+import json
+import random
 
+#read token, chat_id
 with open("./token.txt") as f:
     lines = f.readlines()
     
@@ -7,6 +10,19 @@ with open("./token.txt") as f:
 token = lines[0].strip()
 chat_id = lines[1].strip()
 
+#create bot
 bot = telegram.Bot(token=token)
 
-bot.sendMessage(chat_id=chat_id, text="did it work?")
+#read note.json
+notefile = open('note.json', encoding="utf-8")
+note = json.load(notefile)
+#print(note), 딕셔너리
+f.close()
+
+#데이터 선택 방법 print(data[1]["type"]) 
+#print(len(note))
+#오늘의 픽 뽑기 - random 사용 
+pic = random.randrange(1, len(note))
+
+#sending message
+bot.sendMessage(chat_id=chat_id, text=note[pic]["say"])
